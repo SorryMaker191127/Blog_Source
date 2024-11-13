3a26ab6318b559223844076179092ec61c9f4154
SorryMaker_PC <SorryMaker191127@outlook.com>
2024-11-13 11:36:48 +0800

    构建博客项目框架

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,175 @@
+module.exports = {
+  title: "SorryMaker",
+  description: "Just playing around",
+  themeConfig: {
+    logo: "/assets/img/head.png",
+    nav: [
+      {
+        text: "JavaScript",
+        items: [
+          {
+            text: "基本概念",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "引用类型",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "面向对象的程序设计",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "函数表达式",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "BOM",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "客户端检测",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "DOM",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "DOM扩展",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "DOM2和DOM3",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "事件",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "表单脚本",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "使用Canvas绘图",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "HTML5脚本编程",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "错误处理与调试",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "Javasript与XML",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "E4X",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "JSON",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "Ajax与Comet",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "高级技巧",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "离线应用与客户端存储",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "最佳实践",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+          {
+            text: "新兴API",
+            items: [
+              { text: "数据类型", link: "/JavaScript/#数据类型" },
+              { text: "Boolean", link: "/JavaScript/#boolean-类型" },
+            ],
+          },
+        ],
+      },
+      { text: "Vue2", link: "/Vue2/" },
+      { text: "Vue3", link: "/Vue3/" },
+    ],
+    sidebar: {
+      "/JavaScript/": ["", "chapter01", "chapter02"],
+      "/Vue2/": ["", "chapter01", "chapter02"],
+    },
+    sidebarDepth: 2,
+  },
+};
---
+++ docs/JavaScript/README.md
@@ -0,0 +1,13 @@
+# 基本概念
+
+## 数据类型
+
+### typeof 操作符
+
+### Undefined 类型
+
+### Null 类型
+
+### Boolean 类型
+
+### Number 类型
